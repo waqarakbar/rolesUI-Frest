a218e799fbdc7f723ab7ef018590b1ced5548ad3
Qazi Hamayun <hamayun4u2004@gmail.com>
2023-03-07 12:10:34 +0500

    changing app id in config

--- Modules/Vms/Config/config.php
+++ Modules/Vms/Config/config.php
@@ -2,7 +2,7 @@
 
 return [
     'name' => 'Vms',
-    'app_id' => 11,
+    'app_id' => 12,
     'active_layout' => 'app_screen_frest',
     'company_title' => 'Organization',
     'section_title' => 'Unit'
